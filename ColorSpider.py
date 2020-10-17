7b707024017d114cf16df5cc40db6eb320d96a6f
Yuqing_Chen <chenyuqing0429@foxmail.com>
2020-10-17 22:27:03 +0800

    - 颜色爬虫更新了去重功能 - 完善了颜色词库

--- ColorSpider.py
+++ ColorSpider.py
@@ -4,11 +4,15 @@ from bs4 import BeautifulSoup
 r = requests.get('https://www.analogouscolors.com/cn/')
 soup = BeautifulSoup(r.text, 'html.parser')
 colorInfo = soup.find_all('img')
+colorSet = set()	# 用于去重
 for color in colorInfo:
 	try:
 		colorStr = color['title']
 		colorStr = colorStr.split(',')
-		print('"'+ colorStr[0] + '"' + ' : ' + '"#' + colorStr[1] + '",')
-		#print(color['title'].replace(',', ' : ') + ',')
+		if colorStr[0] in colorSet:
+			continue
+		else:
+			colorSet.add(colorStr[0])
+			print('"'+ colorStr[0] + '"' + ' : ' + '"#' + colorStr[1] + '",')
 	except:
 		continue
