3b36ea79b53666d3b5d92bea88ca47c836c6f583
Yuqing_Chen <chenyuqing0429@foxmail.com>
2020-12-06 16:38:25 +0800

    Updates

---
+++ precache-manifest.ea92f18d9386f79ada2e123ac390ee30.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44eba6abfbe3cdba1c8879889d940fe",
+    "url": "/Software-Interface-Prototype-Automatic-Generator/index.html"
+  },
+  {
+    "revision": "8d0b942263b4c14e33dd",
+    "url": "/Software-Interface-Prototype-Automatic-Generator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dce131702c896d44edc1",
+    "url": "/Software-Interface-Prototype-Automatic-Generator/static/js/2.d8b9f9d7.chunk.js"
+  },
+  {
+    "revision": "e4d980926fadc05888ff1db78303136f",
+    "url": "/Software-Interface-Prototype-Automatic-Generator/static/js/2.d8b9f9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d0b942263b4c14e33dd",
+    "url": "/Software-Interface-Prototype-Automatic-Generator/static/js/main.1a0bd22e.chunk.js"
+  },
+  {
+    "revision": "06d620f78fb0f55b7e2e",
+    "url": "/Software-Interface-Prototype-Automatic-Generator/static/js/runtime-main.92bdfd00.js"
+  },
+  {
+    "revision": "f3a53693fdbaf052e8f883c8fa2efcd6",
+    "url": "/Software-Interface-Prototype-Automatic-Generator/static/media/blog-post.1.f3a53693.md"
+  },
+  {
+    "revision": "99cbba3ba72c097ec32daa6f5eed33ec",
+    "url": "/Software-Interface-Prototype-Automatic-Generator/static/media/blog-post.2.99cbba3b.md"
+  },
+  {
+    "revision": "8d6e276be069ee011ba792969f01d327",
+    "url": "/Software-Interface-Prototype-Automatic-Generator/static/media/blog-post.3.8d6e276b.md"
+  }
+]);
