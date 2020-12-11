3b1d6131ee3c4e45139cc2720cfaa4716b617cf1
Yuqing_Chen <chenyuqing0429@foxmail.com>
2020-12-11 13:09:42 +0800

    组件化预览页

--- react_component/src/App.js
+++ react_component/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import NaviBar from './NaviBar';
 import Typography from '@material-ui/core/Typography';
-
 import Album from './template/album/Album';
 import Blog from './template/blog/Blog';
 import Checkout from './template/checkout/Checkout';
@@ -14,24 +13,11 @@ import SignIn from './template/sign-in/SignIn';
 import SignInSide from './template/sign-in-side/SignInSide';
 import SignUp from './template/sign-up/SignUp';
 import StickyFooter from './template/sticky-footer/StickyFooter';
-import { AppBar, Divider, Drawer, IconButton, Link, Toolbar } from '@material-ui/core';
+import { AppBar, IconButton, Toolbar } from '@material-ui/core';
 import GridLayout from 'react-grid-layout';
 import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import List from '@material-ui/core/List';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import FiberNewIcon from '@material-ui/icons/FiberNew';
-import BorderAllIcon from '@material-ui/icons/BorderAll';
-import CallSplitIcon from '@material-ui/icons/CallSplit';
-import ColorizeIcon from '@material-ui/icons/Colorize';
-import Box from '@material-ui/core/Box';
-import VisibilityIcon from '@material-ui/icons/Visibility';
-import Skeleton from '@material-ui/lab/Skeleton';
 import ToolBar from './ToolBar';
+import Preview from './Preview';
 
 
 
@@ -45,10 +31,6 @@ export default function App() {
     0 - Default
   */
   const [template, setTemplate] = useState(0);
-
-  const theme = useTheme();
-
-  const drawerWidth = "25%";
 
   const useStyles = makeStyles((theme) => ({
     NaviBar: {
@@ -79,10 +61,6 @@ export default function App() {
     BarTitle: {
       flexGrow: 1
     },
-
-    DrawerPaper: {
-      width: drawerWidth
-    }
   }));
 
   const classes = useStyles();
@@ -107,97 +85,11 @@ export default function App() {
           <NaviBar setStatus={setStatus} setRenderBg={setRenderBg} setTemplate={setTemplate}>
           </NaviBar>
         </div>
-        <Drawer
-          variant="persistent"
-          anchor="right"
+        <Preview
           open={drawerOpen}
-          classes={{
-            paper: classes.DrawerPaper,
-          }}
+          setDrawer={setDrawer} 
         >
-          <div>
-            <IconButton onClick={() => {
-              setDrawer(false);
-            }}>
-              {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-            </IconButton>
-            <Divider />
-            <List>
-
-              <ListItem button key={'function'}>
-                <ListItemIcon>
-                  <FiberNewIcon />
-                </ListItemIcon>
-                <ListItemText primary={'需求功能'}>
-                </ListItemText>
-                <ListItemText primary={'个人博客'}>
-                </ListItemText>
-              </ListItem>
-
-              <ListItem button key={'layout'}>
-                <ListItemIcon>
-                  <BorderAllIcon />
-                </ListItemIcon>
-                <ListItemText primary={'采取的布局'}>
-                </ListItemText>
-                <ListItemText primary={'中心舞台'}>
-                </ListItemText>
-              </ListItem>
-
-              <ListItem button key={'navigation'}>
-                <ListItemIcon>
-                  <CallSplitIcon />
-                </ListItemIcon>
-                <ListItemText primary={'导航模式'}>
-                </ListItemText>
-                <ListItemText primary={'金字塔模式'}>
-                </ListItemText>
-              </ListItem>
-
-              <ListItem button key={'color'}>
-                <ListItemIcon>
-                  <ColorizeIcon />
-                </ListItemIcon>
-                <ListItemText primary={'Primary Color'}>
-                </ListItemText>
-                <Box bgcolor="primary.main" p={2} m={1}>
-                </Box>
-              </ListItem>
-
-              <ListItem button key={'color'}>
-                <ListItemIcon>
-                  <ColorizeIcon />
-                </ListItemIcon>
-                <ListItemText primary={'Secondary Color'}>
-                </ListItemText>
-                <Box bgcolor="secondary.main" p={2} m={1}>
-                </Box>
-              </ListItem>
-
-              <ListItem button key={'preview'}>
-                <ListItemIcon>
-                  <VisibilityIcon />
-                </ListItemIcon>
-                <ListItemText primary={'预览效果'}>
-                </ListItemText>
-                <Skeleton animation="wave" width={40} height={40}>
-                </Skeleton>
-              </ListItem>
-
-              <Divider />
-
-              <ListItem key={'github'}>
-                <ListItemIcon>
-                  <GitHubIcon />
-                </ListItemIcon>
-                <Link href="https://github.com/scyq/Software-Interface-Prototype-Automatic-Generator">
-                  View On GitHub
-                </Link>
-              </ListItem>
-
-            </List>
-          </div>
-        </Drawer>
+        </Preview>
       </Container>
     );
   }
